a88407005105110d4bb3193490134be1b3e37b82
Kira Clements <kiwi.klc@gmail.com>
2023-02-10 18:23:45 -0700

    unit-5

--- project-5/crud.py
+++ project-5/crud.py
@@ -4,19 +4,27 @@ from model import db, User, Movie, Rating, connect_to_db
 
 def create_user(email, password):
     """Create and return a new user."""
-    user2 = User(
+    user = User(
         email=email,
         password=password
         )
-    return user2
+    return user
+
 
 def get_users():
     """returns all users"""
     return User.query.all()
 
+
 def get_user_by_id(user_id):
     """returns user's details through id"""
     return User.query.get(user_id)
+
+
+def get_user_by_email(email):
+    """returns a user based on email"""
+    return User.query.filter(User.email == email).first()
+
 
 def create_movie(title, overview, release_date, poster_path):
     """creates and returns a movie"""
@@ -33,9 +41,11 @@ def get_movies():
     """returns all movies"""
     return Movie.query.all()
 
+
 def get_movie_by_id(movie_id):
     """returns a movie's details through id"""
     return Movie.query.get(movie_id)
+
 
 def create_rating(user, movie, score):
     """creates and returns a rating"""
@@ -45,6 +55,11 @@ def create_rating(user, movie, score):
         score=score
         )
     return rating1
+
+
+def get_ratings_by_user(user):
+    user = User.query.get(user)
+    return user.ratings
 
 
 if __name__ == '__main__':
--- project-5/server.py
+++ project-5/server.py
@@ -14,8 +14,17 @@ app.jinja_env.undefined = StrictUndefined
 @app.route("/")
 def homepage():
     """view homepage"""
+    logged_in = session.get("user_id")
 
-    return render_template("homepage.html")
+    if logged_in:
+        return redirect("/home")
+    else:
+        return render_template("homepage.html")
+
+@app.route("/home")
+def home():
+    """view home logged in"""
+    return render_template("home_logged_in.html")
 
 @app.route("/movies")
 def view_movies():
@@ -44,6 +53,97 @@ def user_details(user_id):
 
     user = crud.get_user_by_id(user_id)
     return render_template("show_user.html", user=user)
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """creates a new user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    email_check = crud.get_user_by_email(email)
+    
+    if email_check:
+        flash("Try again, that user already exists")
+    else:
+        user1 = crud.create_user(email, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("user successfully added")  
+    return redirect("/")
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    """logs in a user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    user = crud.get_user_by_email(email)
+    
+    if user:
+        if user.password == password:
+            session["user_id"] = user.user_id
+            flash("successfully logged in") 
+        else:
+            flash("login information doesn't match, please try again.")
+            flash(f"{user.password}")
+            flash(f"{password}")
+            return redirect("/")
+    else:
+        flash("login information doesn't match, please try again.")
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    """logs the user out"""
+
+    del session["user_id"]
+    flash("you successfully logged out!")
+    return redirect("/")
+
+@app.route("/movies/<movie_id>/rate")
+def rate(movie_id):
+
+    logged_in = session.get("user_id")
+
+    if logged_in:
+        movie = crud.get_movie_by_id(movie_id)
+        return render_template("rate_movie.html", movie=movie)
+    else:
+        flash("please log in to access this feature")
+        return redirect("/")
+
+@app.route("/movies/<movie_id>/rate", methods=["POST"])
+def rated(movie_id):
+    """adds a rating to database"""
+    logged_in = session.get("user_id")
+
+    if logged_in:
+        score = request.form.get("radio_rating")
+        user = crud.get_user_by_id(logged_in)
+        movie = crud.get_movie_by_id(movie_id)
+
+        movie_rating = crud.create_rating(user, movie, score)
+
+        db.session.add(movie_rating)
+        db.session.commit()
+        return redirect("/")
+    else:
+        flash("please log in to access this page")
+        return redirect("/")
+
+@app.route("/ratings")
+def view_ratings():
+    """view ratings"""
+
+    logged_in = session.get("user_id")
+
+    if logged_in:
+        user_id = session["user_id"]
+        ratings = crud.get_ratings_by_user(user_id)
+        return render_template("view_ratings.html", ratings=ratings)
+    else:
+        flash("please log in to access this feature")
+        return redirect("/")
 
 
 if __name__ == "__main__":
